e1df7836ecb3d98f5a27e7424ee6dc140095bdc0
Alberto Fabian <lf_alberto@hotmail.com>
2023-09-18 13:53:11 -0600

    primer commir

---
+++ index.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f24eb69f99.js" crossorigin="anonymous"></script>
+    <title>Hello, world!</title>
+    <style>
+        .c-pos {
+            margin-top: 200px;
+        }
+
+        .text-pos-3 {
+            font-size: 20px;
+            color: #64748b;
+            font-weight: 400;
+        }
+
+        .text-pos-4 {
+            font-size: 20px;
+            color: #0F172B;
+            font-weight: 400;
+        }
+
+        .text-pos-2 {
+            font-size: 40px;
+            color: #0F172B;
+            font-weight: 400;
+        }
+
+
+        .custom-list {
+            list-style: none;
+            /* Elimina los puntos de la lista */
+        }
+
+        .custom-list i {
+            color: #005FF9;
+            /* Cambia el color de los íconos */
+            margin-right: 10px;
+            /* Agrega un espacio entre el ícono y el texto */
+        }
+    </style>
+</head>
+
+<body style="background-color:#F1F5F9">
+
+    <nav class="navbar fixed-top navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand text-center" href="#">Llamanos al (734) 100 6945 -Lunes a Viernes 8:00 a 19:00 Sabado 8:00 a 12:00</a>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mt-5 fixed-top ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://softmorpos.com/img/logo.png" alt="" width="150" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Soluciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Planes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button> -->
+                    <button class="btn btn-outline-dark m-1">Ingresa</button>
+                    <button class="btn btn-primary m-1">Empieza gratis</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container c-pos">
+        <div class="row">
+            <div class="col-md-6 text-pos-c">
+                <h3 class="text-pos-3">Solución de Punto de Venta Especializada para Taller de Reparaciones</h3>
+                <h2 class="text-pos-2">
+                    ¿Necesitas vender y generar notas en cuestión de segundos? <br> ¡Con <strong class="text-primary">Softmor POS </strong> , es posible!
+                </h2>
+                <h6 class="text-pos-3">
+                    Optimiza tu proceso de ventas y brinda a tus clientes una experiencia de compra única y rápida. Di adiós a las largas colas en tus momentos más concurridos.
+                </h6>
+                <button class="btn btn-lg btn-primary mt-5" type="button">Empieza gratis</button>
+
+                <h5>Software de 5 entrellas
+                    <img src="./icons8-cinco-de-cinco-estrellas-100.png" width="120px" alt="">
+                </h5>
+
+
+            </div>
+            <div class="col-md-6">
+                <img src="./img_portada_sp_4.png" alt="" class="img-fluid w-100">
+            </div>
+        </div>
+
+        <div class="row mt-5 mb-5">
+            <div class="col-md-4">
+                <div class="card">
+                    <center><img class="m-2" src="./icons8-nube-100.png" width="80" alt=""></center>
+                    <div class="card-body text-center">
+                        <h4 class=" mb-4 card-title">Sin descargas</h4>
+                        <p class=" mb-4 card-text">Accede desde cualquier dispositivo y lugar, manteniendo tu información segura en la nube.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <center> <img class="m-2" src="./icons8-soporte-en-línea-80.png" width="80" alt=""></center>
+                    <div class="card-body text-center">
+                        <h4 class=" mb-4 card-title">Soporte tecnico</h4>
+                        <p class=" mb-4 card-text">Recibe asesoría gratuita de un equipo especializado a través de teléfono, correo electrónico o chat.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <center><img class="m-2" src="./icons8-actualizaciones-disponibles-80.png" width="80" alt=""></center>
+                    <div class="card-body text-center">
+                        <h4 class=" mb-4 card-title">Actualizaciones</h4>
+                        <p class=" mb-4 card-text">Disfruta de actualizaciones constantes sin costos adicionales y mantente siempre al día.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2 class="text-pos-2">
+                    Gestiona tus servicios técnicos con <br> ¡Con la<strong class="text-primary"> mejor opción </strong> en software.
+                </h2>
+            </div>
+            <div class="col-md-6">
+                <img src="./imagen_3.png" alt="" class="img-fluid w-100">
+            </div>
+            <div class="col-md-6 text-center mt-5 d-flex justify-content-center align-items-center flex-column">
+                <h6 class="text-pos-4 mb-5">Taller Simplificado: Softmor Pos para una Administración Eficiente</h6>
+                <ul class="custom-list">
+                    <li><i class="fas fa-check"></i> Acceso completo desde cualquier dispositivo.</li>
+                    <li><i class="fas fa-check"></i> Gestión eficiente desde cualquier lugar.</li>
+                    <li><i class="fas fa-check"></i> Simplifica la administración de talleres.</li>
+                </ul>
+                <button class="btn btn-lg btn-primary " type="button">Empieza gratis</button>
+            </div>
+        </div>
+        <div class="row">
+
+
+            <div class="col-md-6 text-center mt-5 d-flex justify-content-center align-items-center flex-column">
+                <h6 class="text-pos-4 mb-5">Transforma tu Negocio con Softmor Pos</h6>
+                <ul class="custom-list">
+                    <li><i class="fas fa-check"></i> Optimiza tu gestión empresarial con reportes detallados.</li>
+                    <li><i class="fas fa-check"></i> Controla tu inventario de forma eficiente y precisa..</li>
+                    <li><i class="fas fa-check"></i> Potencia tus ventas con un sistema de punto de venta confiable.</li>
+                </ul>
+                <button class="btn btn-lg btn-primary mt-4" type="button">Empieza gratis</button>
+            </div>
+
+            <div class="col-md-6">
+                <img src="./imagen_4.png" alt="" class="img-fluid w-100">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="p-5 mb-4 bg-primary rounded-3">
+                    <div class="container-fluid py-5">
+                        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+                        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
